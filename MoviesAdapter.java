c580d08f82794793cae78d4717ea9f5c789a4003
Bettykirii <kenakirii@gmail.com>
2017-10-07 13:37:42 +0300

    My movie app

--- MoviesAdapter.java
+++ MoviesAdapter.java
@@ -1,5 +1,6 @@
 package com.example.student.movieapp.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import com.bumptech.glide.Glide;
 import com.example.student.movieapp.R;
 import com.example.student.movieapp.model.Movie;
 import com.example.student.movieapp.model.MovieResult;
+import com.example.student.movieapp.model.Result;
 
 import java.util.List;
 
@@ -20,7 +22,8 @@ import java.util.List;
  */
 public class MoviesAdapter  extends RecyclerView.Adapter<MoviesAdapter.MyViewHolder> {
 
-    private List<MovieResult> moviesList;
+    private List<Result> moviesList;
+    private String posterPath = "http://image.ttmdb.org/t/p/w500";
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView title, year, genre;
@@ -37,9 +40,10 @@ public class MoviesAdapter  extends RecyclerView.Adapter<MoviesAdapter.MyViewHol
         }
     }
 
-
-    public MoviesAdapter(List<MovieResult> moviesList) {
+    private Context mContext;
+    public MoviesAdapter(Context context, List<Result> moviesList) {
         this.moviesList = moviesList;
+        mContext = context;
     }
 
     @Override
@@ -52,10 +56,14 @@ public class MoviesAdapter  extends RecyclerView.Adapter<MoviesAdapter.MyViewHol
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        MovieResult movie = moviesList.get(position);
+        Result movie = moviesList.get(position);
         holder.title.setText(movie.getTitle());
         holder.genre.setText(movie.getOverview());
         holder.year.setText(movie.getReleaseDate());
+
+        Glide.with(mContext)
+                .load(posterPath + movie.getPosterPath())
+                .into(holder.posterImageView);
     }
 
     @Override
